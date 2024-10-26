49345d5bbc2296ce46542c51c2800c6c2b9fd578
casecasee <cassey.low.si.yun@gmail.com>
2024-10-26 23:29:08 +0800

    made the popup a form and broke everything else sadge

--- public/attractionImages.js
+++ public/attractionImages.js
@@ -3,22 +3,24 @@ const app = Vue.createApp({
         return { 
               all : [], // list of nearby attractions gotten using google place api
               display_it: false,
-              selected_a: ''
+              selected_a: '', 
+              imgurl: ''
         };
     }, // data
     computed: { 
-        country() { // for use to display header and to pass in to google place api
+        country() { // for us to display header and to pass in to google place api
             const selectedCountry = localStorage.getItem('selectedCountry');
             return selectedCountry;
         } 
     }, // computed
 
     methods: {
-        popup(a_name) {
+        popup(imgurl, a_name) {
             console.log(a_name)
             this.display_it = true;
             this.selected_a = a_name;
-            // change display_it to true and pass it to the component
+            console.log(imgurl);
+            this.imgurl = imgurl;
         }, 
         close() {
             this.display_it = false;
@@ -64,19 +66,19 @@ app.component('country-images', {
         if (result[0]) { // checks if the wiki page exists
             const thumbimage = result[0].thumbnail?.url; // Optional chaining for handling missing thumbnails
             const description = result[0].description || "No description available";
-            // [ ] check if wiki page consists of coords
+            // TODO check if wiki page consists of coords
 
     
             if (thumbimage) {
-                const image = thumbimage.replace('/thumb', '').replace(/\/\d+px-.+$/, '');
+                const image = thumbimage.replace('/thumb', '').replace(/\/\d+px-.+$/, ''); // get big image
                 this.imgurl = image;
                 this.desc = description         
             }
         }
     },
-    
+    // TODO make it a form lol
     template: `
-        <div class='card' v-if=imgurl @click=$emit('popup')>
+        <div class='card' v-if=imgurl @click="$emit('popup', this.imgurl)">
             <img :src=imgurl></img>
             <div class='caption'> 
                 {{ a_name }} 
@@ -85,41 +87,52 @@ app.component('country-images', {
         </div>
     `
     });
-    // component must be declared before app.mount(...)
 
     // [ ] to add trip to database idk if component is gud idea
-    //
     app.component('pop-up', { 
-        props: [ 'a_name', 'display_it' ],
-        
-        data() {
-            return {
-            }
-        }, // data
-        
-        methods: {
-            methodName() {
-                
-            }
-        }, // methods
+        props: [ 'a_name', 'display_it', 'bg_img' ],
         
         template: `
             <div v-if="display_it" class='pop'>
                 <div class='content'>
+
                     <p>{{ a_name }}</p>
-                    <p @click=$emit('close')>&times;</p>
+                    <span id="cancel" @click="$emit('close')">&times;</span>
+
+                    <div class='form-container'>
+                        <form id="attractionForm">
+
+                            <div class="form-group">
+                                <label for="dateSelect">Date:</label>
+                                <div class="row">
+                                    <div class="col">
+                                        <input type="date" id="startDate" name="startDate" class="form-control" required>
+                                    </div>
+                                </div>
+                            </div>
+                    
+                            <div class="form-group">
+                                <label for="cost">Cost:</label>
+                                <div class="row">
+                                    <div class="col-md">
+                                        <input type="number" id="cost" name="cost" class="form-control" placeholder="Cost" required>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </form>
+                    </div>
+
+
                 </div>
 
-
-            </div>
-        
+            </div> 
         `
     });
     // component must be declared before app.mount(...)
     
     const vm = app.mount('#app'); 
-
-// when created we have to get all the attractions and all the info
 
 async function getCoordinates(name) {
     const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(name)}&key=AIzaSyBqbv7BqX1_7CcpPT80locaYJ3fnsdC-Sg`;
@@ -133,92 +146,6 @@ async function getCoordinates(name) {
         long: location.lng
     };
 }
-
-// ------------------------------------- without description ----------------------------------------------------------------------
-// async function getCoordinates(name) {
-//     const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(name)}&key=AIzaSyBqbv7BqX1_7CcpPT80locaYJ3fnsdC-Sg`;
-
-//     const response = await fetch(url);
-//     const data = await response.json();
-
-//     const location = data.results[0].geometry.location;
-//     return {
-//         lat: location.lat,
-//         long: location.lng
-//     };
-// }
-
-// async function getImgs() {
-//     const selectedCountry = localStorage.getItem('selectedCountry');
-//     document.getElementById('countryName').innerText = `Attractions in ${selectedCountry}`;
-
-//     const info = await getCoordinates(selectedCountry);
-//     const lat = info.lat;
-//     const long = info.long;
-
-//     var loc = new google.maps.LatLng(lat, long);
-//     const service = new google.maps.places.PlacesService(document.createElement('div'));
-
-//     const request = {
-//         location: loc,
-//         radius: '100000',
-//         type: ['tourist_attraction', 'museum', 'stadium', 'landmark', 'natural_feature'], 
-//     };
-
-//     service.nearbySearch(request, (results, status) => handleResponse(results, status, selectedCountry));
-// }
-
-// async function handleResponse(results, status, country) {
-//     const destination = document.getElementById('imgs');
-//     destination.innerHTML = '';
-//     const displayedNames = new Set(); 
-
-//     if (status === google.maps.places.PlacesServiceStatus.OK) {
-//         results.forEach(attraction => {
-//             const name = attraction.name;
-//             console.log(name);
-
-//             if (displayedNames.has(name)) {
-//                 return; 
-//             }
-//             displayedNames.add(name); 
-
-// // ---------------------------------------- using wikipedia api --------------------------------------------
-
-//     const url = 'https://api.wikimedia.org/core/v1/wikipedia/en/search/page';
-
-//     axios.get(url, { params: {q : name, limit: 1} })
-//     .then(response => {
-
-//         const result = response.data.pages;
-//         if (result[0]) { // checks if the wiki page exists 
-//             const thumbimage = result[0].thumbnail.url; // TODO case where url is null
-//             image = thumbimage.replace('/thumb', '').replace(/\/\d+px-.+$/, ''); 
-
-//             const card = document.createElement('div');
-//             card.className = 'card'; 
-
-//             const imgElement = document.createElement('img');
-//             imgElement.setAttribute('src', image);
-
-//             const caption = document.createElement('div');
-//             caption.className = 'caption'; 
-//             caption.innerText = name; 
-            
-//             card.appendChild(imgElement);
-//             card.appendChild(caption);
-
-//             destination.appendChild(card);
-//         }
-
-//     })
-//     .catch(error => {
-//         console.error('Error:', error);
-//     });
-
-// })};
-// }
-// ---------------------------------------- end -----------------------------------------------------------------------
 
 // ---------------------------------------- using unsplash api --------------------------------------------
 
@@ -257,38 +184,4 @@ async function getCoordinates(name) {
 
             // })};
                 
-// ---------------------------------------- end -----------------------------------------------------------------------
-
-// ---------------------------------------- using gmaps api -----------------------------------------------------------
-                
-    //         const url = `https://www.googleapis.com/customsearch/v1?key=AIzaSyAnMQUrtx33-PNEd45HT4v5fiPI3I4qyVY&cx=e0d39b341ae854a03&q=${name}`;
-    //         axios.get(url)
-    //             .then(response => {
-    //                 if (response.data.items && response.data.items.length > 0) {
-    //                     const result = response.data.items[0];
-    //                     const image = result.pagemap.cse_image[0].src;
-
-    //                     const card = document.createElement('div');
-    //                     card.className = 'card'; 
-
-    //                     const imgElement = document.createElement('img');
-    //                     imgElement.setAttribute('src', image);
-
-    //                     const caption = document.createElement('div');
-    //                     caption.className = 'caption'; 
-    //                     caption.innerText = name; 
-                        
-    //                     card.appendChild(imgElement);
-    //                     card.appendChild(caption);
-
-    //                     destination.appendChild(card);
-    //                 }
-    //             })
-    //             .catch(error => {
-    //                 console.log(error.message);
-    //             });
-    //     });
-    // } else {
-    //     console.error('PlacesService search failed due to:', status);
-    // }
 // ---------------------------------------- end -----------------------------------------------------------------------
