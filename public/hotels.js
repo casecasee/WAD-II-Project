b3154ebe402198fdc5cbcd2674cdf0c14702a87b
huisiii <huisi.wong.2023@scis.smu.edu.sg>
2024-10-27 02:12:39 +0800

    update

--- public/hotels.js
+++ public/hotels.js
@@ -1,26 +1,17 @@
-// testing
-function updateBudget() {
-    const budget = parseFloat(document.getElementById('budget').value);
-    const spending = parseFloat(document.getElementById('spending').value);
-    
-    // Calculate percentage of budget used
-    const percentage = (spending / budget) * 100;
-    
-    // Cap the percentage at 100 to prevent overflow
-    const cappedPercentage = Math.min(percentage, 100);
-  
-    // Update the circular progress bar
-    const progressRing = document.querySelector('.progress-ring-circle');
-    progressRing.style.background = `conic-gradient(#4caf50 ${cappedPercentage}%, #ccc ${cappedPercentage}% 100%)`;
-    
-    // Update the text inside the ring
-    document.getElementById('progress-value').innerText = `${Math.round(cappedPercentage)}%`;
-  }
+import axios from 'axios';
 
-function addHotel() {
-  const button = document.createElement('button');
-  button.className= "book-btn";
-  button.textContent = "Book";
-  button.setAttribute('aria-label', 'Book Now');
-  return button;
+const options = {
+  method: 'GET',
+  url: 'https://hotels4.p.rapidapi.com/v2/get-meta-data',
+  headers: {
+    'x-rapidapi-key': 'NOKEY',
+    'x-rapidapi-host': 'NOHOST'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
 }
