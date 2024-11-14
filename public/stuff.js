944163ace0efa21b1dc62a88c5c56534a74ecee9
casecasee <cassey.low.si.yun@gmail.com>
2024-11-14 11:24:17 +0800

    changed firebase project

--- public/stuff.js
+++ public/stuff.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB5mhW4Fc3qXg2aFDgRw_ygdQHevo6s2ww",
-  authDomain: "wad2-pt2.firebaseapp.com",
-  projectId: "wad2-pt2",
-  storageBucket: "wad2-pt2.firebasestorage.app",
-  messagingSenderId: "712139358382",
-  appId: "1:712139358382:web:0c1ea26da3db6d11d3ad3f"
+  apiKey: "AIzaSyBpAHShcvt0z8ZmLXRkBPMiNToFVoXru0o",
+  authDomain: "roameo-wad2.firebaseapp.com",
+  projectId: "roameo-wad2",
+  storageBucket: "roameo-wad2.firebasestorage.app",
+  messagingSenderId: "186295026722",
+  appId: "1:186295026722:web:ddcb6b60f6988d415f767b"
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
