329e136f4cf0c8ed3b35b10bc81ee07e399139ac
huisiii <huisi.wong.2023@scis.smu.edu.sg>
2024-11-14 13:57:00 +0800

    UPDATE STUFF.JS FIREBASE CONFIG

--- public/stuff.js
+++ public/stuff.js
@@ -1,12 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import { config } from "./config.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBpAHShcvt0z8ZmLXRkBPMiNToFVoXru0o",
-  authDomain: "roameo-wad2.firebaseapp.com",
-  projectId: "roameo-wad2",
-  storageBucket: "roameo-wad2.firebasestorage.app",
-  messagingSenderId: "186295026722",
-  appId: "1:186295026722:web:ddcb6b60f6988d415f767b"
+  apiKey: config.FIREBASE_API_KEY,
+  authDomain: config.FIREBASE_AUTH_DOMAIN,
+  projectId: config.FIREBASE_PROJECT_ID,
+  storageBucket: config.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: config.FIREBASE_MESSAGING_SENDER_ID,
+  appId: config.FIREBASE_APP_ID
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
